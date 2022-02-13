a1284df30d9f230dda0df4d576e8da5b2d8c972c
Brad Garropy <bradgarropy@gmail.com>
2022-02-13 14:03:37 -0600

    better example. update dependencies.

--- example/plopfile.js
+++ example/plopfile.js
@@ -1,5 +1,7 @@
 const config = plop => {
+    plop.load("@bradgarropy/plop-generator-react-component")
     plop.load("../.")
+    plop.load("@bradgarropy/plop-generator-react-context")
 }
 
 module.exports = config
